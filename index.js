7d695f25de60c4a6e4b4d6683c4922698d06a371
Antony Kamau <kariuantony@gmail.com>
2023-09-26 10:14:27 +0000

    ip blocked, reinitializing new vm

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ import { load } from "cheerio";
 import axios from "axios";
 import { appendFileSync } from "fs";
 
-async function performScraping() {
-  // downloading the target web page
-  // by performing an HTTP GET request in Axios
-  const axiosResponse = await axios.request({
+async function fetchSite(url) {
+  const data = await axios.request({
     method: "GET",
-    url: "https://www.craigslist.org/about/sites",
+    url,
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
     },
   });
 
-  const $ = load(axiosResponse.data);
+  return data;
+}
 
-  // initializing the data structures
-  // that will contain the scraped data
+async function scrapeSiteUrls() {
+  const sitesPage = await fetchSite("https://www.craigslist.org/about/sites");
+  const $ = load(sitesPage.data);
   const sites = [];
 
   // .colmask -> country container
@@ -29,7 +29,6 @@ async function performScraping() {
     .find(".colmask")
     .each((index, country) => {
       // Country
-
       $(country)
         .find(".box")
         .each((index, box) => {
@@ -42,14 +41,47 @@ async function performScraping() {
                 .find("a")
                 .each((index, url) => {
                   const urls = $(url).attr("href");
-                  console.log(urls);
-
+                  sites.push(urls);
                 });
             });
         });
     });
 
-  console.log(sites);
+  return sites;
 }
 
-performScraping();
+const sites = await scrapeSiteUrls();
+console.log(sites)
+
+// sites.map(async (url) => {
+//   // to follow /search/hum
+//   const searchSite = await fetchSite(`${url}/search/sof`);
+//   const $ = load(searchSite.data);
+
+//   $("body")
+//     .find(".posting-title")
+//     .each((index, titleLink) => {
+//       const link = $(titleLink).attr("href");
+//       console.log(link);
+//     });
+
+//   // to follow /search/sof
+//   // to follow /search/sad
+//   // to follow /search/tch
+//   // to follow /search/web
+//   // to follow /search/cpg
+// });
+
+// for each site append search to url to follow e.g sandiego.craiglist.org/search/hum for human resource
+
+// 714 urls to follow
+// areas of interest
+// Jobs -> software / qa / dba
+//      -> web info design
+//      -> technical support
+//      -> Systems / networking
+//      -> human resource
+// Gigs -> computer
+
+// TODO! the grub every link in view and follow
+// TODO? grub Title, subheading, body, posted date, post id, compensation, employment type
